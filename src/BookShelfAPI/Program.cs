d1264f508c5d0c3b71748701f0d5c8313b0cb86d
BartoszBaniak <bartosz.baniak09@gmail.com>
2026-04-29 19:59:10 +0200

    Add Swagger / OpenAPI

--- src/BookShelfAPI/Program.cs
+++ src/BookShelfAPI/Program.cs
@@ -14,9 +14,24 @@ builder.Services.AddInfrastructure(connectionString);
 builder.Services.AddProblemDetails();
 builder.Services.AddExceptionHandler<DomainExceptionHandler>();
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new()
+    {
+        Title = "BookShelf API",
+        Version = "v1",
+        Description = "Personal book library management API.",
+    });
+});
+
 var app = builder.Build();
 
 app.UseExceptionHandler();
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
 app.MapControllers();
 
 app.Run();
